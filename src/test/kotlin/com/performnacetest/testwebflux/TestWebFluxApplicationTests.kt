ab91a510ada9bfd2284df2b6cde54d5dd451a3d8
luke-igaw <luke.kim@igaworks.com>
2024-03-18 16:17:06 +0900

    Initial commit

---
+++ src/test/kotlin/com/performnacetest/testwebflux/TestWebFluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.performnacetest.testwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestWebFluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
