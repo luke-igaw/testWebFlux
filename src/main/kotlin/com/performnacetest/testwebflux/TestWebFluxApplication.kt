dabaad58b9552abfad3240dc45f847439435f951
luke-igaw <luke.kim@igaworks.com>
2024-03-18 18:24:29 +0900

    Connect to local mongo db

--- src/main/kotlin/com/performnacetest/testwebflux/TestWebFluxApplication.kt
+++ src/main/kotlin/com/performnacetest/testwebflux/TestWebFluxApplication.kt
@@ -2,8 +2,10 @@ package com.performnacetest.testwebflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableMongoRepositories
 class TestWebFluxApplication
 
 fun main(args: Array<String>) {
